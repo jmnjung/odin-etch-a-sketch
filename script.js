5f1c4e2b79f0e406cb53600e5c950c1186f566f1
Jimin Jung <170469689+jmnjung@users.noreply.github.com>
2024-11-11 23:10:59 -0800

    Add rainbow mode and grid clearing feature

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
 const gridInput = document.querySelector("#size");
+const rainbowBtn = document.querySelector("#rainbow");
+const clearBtn = document.querySelector("#clear");
 
 let gridSize = 16;
 let draw = false;
+let rainbow = false;
+
+function getRandomColor() {
+  const getRandomValue = () => Math.floor(Math.random() * 256);
+  return `rgb(${getRandomValue()}, ${getRandomValue()}, ${getRandomValue()})`;
+}
 
 function handleDraw(e) {
-  if (draw) {
+  if (!draw) {
+    return;
+  } else if (rainbow) {
+    e.target.style.backgroundColor = getRandomColor();
+    e.target.style.opacity = 1;
+  } else if (e.target.style.backgroundColor !== "black") {
     e.target.style.backgroundColor = "black";
+    e.target.style.opacity = "0.1";
+  } else {
+    const currOpacity = parseFloat(e.target.style.opacity);
+    const finalOpacity = Math.min(currOpacity + 0.1, 1);
+    e.target.style.opacity = `${finalOpacity}`;
   }
 }
 
@@ -28,6 +46,14 @@ function deleteGrid() {
   }
 }
 
+function clearGrid() {
+  const squares = document.querySelectorAll(".container > div");
+  squares.forEach((square) => {
+    square.style.opacity = "";
+    square.style.backgroundColor = "";
+  });
+}
+
 body.addEventListener("mousedown", () => {
   draw = true;
 });
@@ -42,5 +68,16 @@ gridInput.addEventListener("change", () => {
   deleteGrid();
   createGrid(gridSize);
 });
+
+rainbowBtn.addEventListener("click", () => {
+  rainbow = !rainbow;
+  if (rainbow) {
+    rainbowBtn.style.backgroundColor = "#e0e0e0";
+  } else {
+    rainbowBtn.style.backgroundColor = "white";
+  }
+});
+
+clearBtn.addEventListener("click", clearGrid);
 
 createGrid(gridSize);
