d7fe48006894c756e237547a6d55750d342b0dc3
Jimin Jung <170469689+jmnjung@users.noreply.github.com>
2024-11-11 16:54:07 -0800

    Implement drawing and grid resizing features

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const gridInput = document.querySelector("#size");
 
 let gridSize = 16;
+let draw = false;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const squareDiv = document.createElement("div");
-  squareDiv.style.height = `${100 / gridSize}%`;
-  squareDiv.style.width = `${100 / gridSize}%`;
-  container.appendChild(squareDiv);
+function handleDraw(e) {
+  if (draw) {
+    e.target.style.backgroundColor = "black";
+  }
 }
+
+function createGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.style.height = `${100 / size}%`;
+    squareDiv.style.width = `${100 / size}%`;
+    squareDiv.addEventListener("mouseenter", handleDraw);
+    container.appendChild(squareDiv);
+  }
+}
+
+function deleteGrid() {
+  while (container.firstChild) {
+    container.firstChild.removeEventListener("mouseenter", handleDraw);
+    container.removeChild(container.firstChild);
+  }
+}
+
+body.addEventListener("mousedown", () => {
+  draw = true;
+});
+
+body.addEventListener("mouseup", () => {
+  draw = false;
+});
+
+gridInput.addEventListener("change", () => {
+  gridSize = Math.min(gridInput.max, Math.max(gridInput.min, gridInput.value));
+  gridInput.value = gridSize;
+  deleteGrid();
+  createGrid(gridSize);
+});
+
+createGrid(gridSize);
